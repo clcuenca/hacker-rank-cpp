86d73d3567e22b9a4828f48990aa97179a371bac
Carlos Cuenca <cuenca.cl@gmail.com>
2020-10-07 17:34:14 -0700

    add a very big sum

---
+++ warmup/diagonaldifference/diagonaldifference.cpp
@@ -0,0 +1,19 @@
+/// Hacker Rank - ACM - Fall 2020
+/// From A to B
+/// Author: Carlos L. Cuenca
+/// Date: 10/04/20
+
+#include<iostream>
+#include<string>
+#include<vector>
+#include<cmath>
+
+/// -----------------
+/// Program Constants
+
+const int SUCCESS = 0;
+
+/// --------------
+/// Driver Program
+
+int main(int argc, char* argv[]) {
